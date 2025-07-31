e4c6d2094dcad84e4ae6e8eecb75ed0a109242f5
AdemolaAl <ademolaalameen86@gmail.com>
2025-07-31 15:23:55 +0100

    Added Dotenv

--- connection.js
+++ connection.js
@@ -1,7 +1,8 @@
 import { Sequelize, DataTypes } from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config({ path: "secrets.env" });
 
-
-const sequelize = new Sequelize('alameen_ecomus', 'alameen_alameen', 'pj^1!aR%d6iL', {
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
   host: '127.0.0.1',
   port: 5522,
   dialect: 'mysql',
