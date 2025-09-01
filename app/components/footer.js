d698211e05efea71570af9c57ce6db37ddcd4f20
AdemolaAl <ademolaalameen86@gmail.com>
2025-09-01 16:57:30 +0100

    Finished Account content

--- app/components/footer.js
+++ app/components/footer.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Footer() {
     return (
         <footer>
-            
+
             <div className="top">
                 <div>
                     <Image
@@ -49,54 +49,57 @@ export default function Footer() {
         </footer>
     )
 }
-export  function GreyFooter() {
+export function GreyFooter() {
     return (
         <footer className='grey'>
-            <div className="top">
-                <div>
-                    <Image
-                        src='/public/ecomus.svg'
-                        alt="Description of the image"
-                        width={0}
-                        height={0}
-                        style={{
-                            width: '150px',
-                            height: 'fit-Content',
-                        }}
-                    />
-                    <p>
-                        Address: 1234 Fashion Street, Suite 567,<br />
-                        New York, NY 10001<br />
-                        Email: info@fashionshop.com<br />
-                        Phone: (212) 555-1234
-                    </p>
-                </div>
+            <div className='cvr'>
+                <div className="top">
+                    <div>
+                        <Image
+                            src='/public/ecomus.svg'
+                            alt="Description of the image"
+                            width={0}
+                            height={0}
+                            style={{
+                                width: '150px',
+                                height: 'fit-Content',
+                            }}
+                        />
+                        <p>
+                            Address: 1234 Fashion Street, Suite 567,<br />
+                            New York, NY 10001<br />
+                            Email: info@fashionshop.com<br />
+                            Phone: (212) 555-1234
+                        </p>
+                    </div>
 
-                <div>
-                    <p>Help</p>
-                    <p>Privacy Policy</p>
-                    <p>Shipping</p>
-                    <p>Terms and condition</p>
-                    <p>FAQ'S</p>
+                    <div>
+                        <p>Help</p>
+                        <p>Privacy Policy</p>
+                        <p>Shipping</p>
+                        <p>Terms and condition</p>
+                        <p>FAQ'S</p>
+                    </div>
+                    <div>
+                        <p>About Us</p>
+                        <p>Our Story</p>
+                        <p>Contact us</p>
+                        <p>Our Stores</p>
+                    </div>
+                    <div className='email'>
+                        <p>Sign Up for Email</p>
+                        <p>Sign up to get first dibs on new arrivals, sales, exclusive content, events and more!</p>
+                    </div>
                 </div>
-                <div>
-                    <p>About Us</p>
-                    <p>Our Story</p>
-                    <p>Contact us</p>
-                    <p>Our Stores</p>
-                </div>
-                <div className='email'>
-                    <p>Sign Up for Email</p>
-                    <p>Sign up to get first dibs on new arrivals, sales, exclusive content, events and more!</p>
+                <div className="bottom">
+                    <p>© 2024 Ecomus Store. All Rights Reserved</p>
                 </div>
             </div>
-            <div className="bottom">
-                <p>© 2024 Ecomus Store. All Rights Reserved</p>
-            </div>
+
         </footer>
     )
 }
-export  function WhiteFooter() {
+export function WhiteFooter() {
     return (
         <footer className='white'>
             <div className="top">
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,27 +1,304 @@
+'use client'
 import Header from "../dashboardComponents/header";
 import Headtext from "../dashboardComponents/headtext";
 import { GreyFooter } from "../components/footer";
 import "../dashboard.scss"
+import { useState } from "react";
 
 export default function () {
-    return(
+
+    const [activeTab, setActiveTab] = useState("dashboard")
+    const [activeAddress, setActiveAddress] = useState("")
+
+    const openTab = (data) => {
+        setActiveAddress('')
+        setActiveTab(data)
+
+    }
+    const openAddress = (data) => {
+        setActiveAddress(data)
+
+    }
+
+
+    const Tab = ({ name, activeTab, openTab, children }) => (
+        <div onClick={() => openTab(name)} style={{ borderLeft: activeTab == name ? '2px solid black' : '' }}>{children}</div>
+    )
+
+
+
+
+    return (
         <div>
             <Header />
             <Headtext text="My account" />
             <div className="dashboard-main">
                 <div className="dash-cover">
                     <div className="navigation">
-                        <div>Dashboard</div>
-                        <div>Orders</div>
-                        <div>Downloads</div>
-                        <div>Addresses</div>
-                        <div>Account details</div>
-                        <div>Log out</div>
+                        <Tab name="dashboard" activeTab={activeTab} openTab={openTab}>Dashboard</Tab>
+                        <Tab name="orders" activeTab={activeTab} openTab={openTab}>Orders</Tab>
+                        <Tab name="addresses" activeTab={activeTab} openTab={openTab}>Addresses</Tab>
+                        <Tab name="account" activeTab={activeTab} openTab={openTab}>Account details</Tab>
+                        <Tab name="logout" activeTab={activeTab} openTab={openTab}>Log out</Tab>
                     </div>
+
                     <div className="content">
-                        <div className="dashboard">
-                            
-                        </div>
+                        {
+                            activeTab == 'dashboard' ?
+                                <div className="dashboard">
+                                    Hello <p className="username"> ademolaalameen86 </p> <br />
+                                    From your account dashboard you can view your <a href=""> recent orders</a>, manage your <a href=""> shipping and billing addresses</a>, and edit your <a href="">password and account details</a>.
+                                </div>
+
+                                : activeTab == 'orders' ?
+                                    <div className="orders">
+                                        {/* You don't have any orders yet. */}
+                                        <div className="orders-details">
+                                            <div className="rowhead row">
+                                                <div>Order</div>
+                                                <div>Date</div>
+                                                <div>Status</div>
+                                                <div>Total</div>
+                                                <div>Actions</div>
+                                            </div>
+
+                                            <div className="row">
+                                                <div>#6924</div>
+                                                <div>September 1, 2025</div>
+                                                <div>On hold</div>
+                                                <div>$28.00 for 1 item</div>
+                                                <div><a href="">View</a></div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+
+
+                                    : (activeTab == 'addresses' && activeAddress == '') ?
+                                        <div className="addresses">
+                                            <p>The following addresses will be used on the checkout page by default.</p>
+
+
+                                            <div>
+                                                <header>
+                                                    <h2>Billing address</h2>
+                                                    <p className="edit" onClick={() => openAddress('billing-address')}>
+                                                        Edit Billing address
+                                                    </p>
+                                                </header>
+                                                <address>
+                                                    Ademola Al-ameen<br />
+                                                    caleb<br />
+                                                    37 Alh.baruwa street<br />
+                                                    Agboju<br />
+                                                    Lagos<br />
+                                                    Lagos<br />
+                                                    102101<br />
+                                                    Nigeria
+                                                </address>
+                                            </div>
+
+                                            <div>
+                                                <header>
+                                                    <h2>Shipping address</h2>
+                                                    <p className="edit" onClick={() => openAddress('shipping-address')}>
+                                                        Edit Shipping address
+                                                    </p>
+                                                </header>
+                                                <address>
+                                                    Ademola Al-ameen<br />
+                                                    caleb<br />
+                                                    37 Alh.baruwa street<br />
+                                                    Agboju<br />
+                                                    Lagos<br />
+                                                    Lagos<br />
+                                                    102101<br />
+                                                    Nigeria
+                                                </address>
+                                            </div>
+
+                                        </div>
+
+                                        : activeAddress == 'billing-address' ?
+                                            <div className="billing-address">
+                                                <h2>Billing Address</h2>
+                                                <form>
+                                                    <div className="names">
+                                                        <label>
+                                                            First name * <br />
+                                                            <input type="text" placeholder="First name *" />
+                                                        </label>
+
+                                                        <label>
+                                                            Last name * <br />
+                                                            <input type="text" placeholder="Last name *" />
+                                                        </label>
+
+
+                                                    </div>
+
+                                                    <label>
+                                                        Company name (optional)<br />
+                                                        <input type="text" name="company" />
+                                                    </label>
+                                                    <br />
+
+                                                    <label>
+                                                        Country / Region *
+                                                        <input type="text" name="country" required />
+                                                    </label>
+                                                    <br />
+
+                                                    <label>
+                                                        Street address *
+                                                        <input type="text" name="street" required />
+                                                    </label>
+                                                    <br />
+
+                                                    <label>
+                                                        Town / City *
+                                                        <input type="text" name="city" required />
+                                                    </label>
+                                                    <br />
+
+                                                    <label>
+                                                        State *
+                                                        <input type="text" name="state" required />
+                                                    </label>
+                                                    <br />
+
+                                                    <label>
+                                                        Phone *
+                                                        <input type="tel" name="phone" required />
+                                                    </label>
+                                                    <br />
+
+                                                    <label>
+                                                        Email address *
+                                                        <input type="email" name="email" required />
+                                                    </label>
+                                                    <br />
+
+                                                    <button type="submit">Save address</button>
+                                                </form>
+                                            </div>
+
+                                            : activeAddress == 'shipping-address' ?
+                                                <div className="billing-address">
+                                                    <h2>Shipping Address</h2>
+                                                    <form>
+                                                        <div className="names">
+                                                            <label>
+                                                                First name * <br />
+                                                                <input type="text" placeholder="First name *" />
+                                                            </label>
+
+                                                            <label>
+                                                                Last name * <br />
+                                                                <input type="text" placeholder="Last name *" />
+                                                            </label>
+
+
+                                                        </div>
+
+                                                        <label>
+                                                            Company name (optional)<br />
+                                                            <input type="text" name="company" />
+                                                        </label>
+                                                        <br />
+
+                                                        <label>
+                                                            Country / Region *
+                                                            <input type="text" name="country" required />
+                                                        </label>
+                                                        <br />
+
+                                                        <label>
+                                                            Street address *
+                                                            <input type="text" name="street" required />
+                                                            <input type="text" name="street2" required />
+                                                        </label>
+                                                        <br />
+
+                                                        <label>
+                                                            Town / City *
+                                                            <input type="text" name="city" required />
+                                                        </label>
+                                                        <br />
+
+                                                        <label>
+                                                            State *
+                                                            <input type="text" name="state" required />
+                                                        </label>
+                                                        <br />
+
+                                                        <br />
+
+                                                        <button type="submit">Save address</button>
+                                                    </form>
+                                                </div>
+
+                                                : activeTab == 'account' ?
+                                                    <div className="billing-address">
+                                                        <form>
+                                                            <div className="names">
+                                                                <label>
+                                                                    First name * <br />
+                                                                    <input type="text" placeholder="First name *" />
+                                                                </label>
+
+                                                                <label>
+                                                                    Last name * <br />
+                                                                    <input type="text" placeholder="Last name *" />
+                                                                </label>
+
+
+                                                            </div>
+
+                                                            <label>
+                                                                Username<br />
+                                                                <input type="text" name="username" placeholder="Username *" />
+                                                            </label>
+                                                            <br />
+                                                            <label>
+                                                                Email<br />
+                                                                <input type="text" name="email" placeholder="Email *" />
+                                                            </label>
+                                                            <br />
+
+                                                            <div className="password">
+                                                                <p>Password change</p>
+                                                                <form>
+                                                                    <label>
+                                                                        Current password (leave blank to leave unchanged)<br />
+                                                                        <input type="password" name="email" placeholder="Email *" />
+                                                                    </label>
+                                                                    <br />
+                                                                    <label>
+                                                                        New password (leave blank to leave unchanged)<br />
+                                                                        <input type="password" name="email" placeholder="Email *" />
+                                                                    </label>
+                                                                    <br />
+                                                                    <label>
+                                                                        Confirm new password<br />
+                                                                        <input type="password" name="email" placeholder="Email *" />
+                                                                    </label>
+                                                                    <br />
+
+
+                                                                </form>
+
+                                                            </div>
+
+                                                            <button type="submit">Save address</button>
+                                                        </form>
+                                                    </div>
+
+
+                                                    : <></>
+
+                        }
+
                     </div>
                 </div>
             </div>
